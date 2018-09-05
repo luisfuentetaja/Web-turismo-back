196ad5896863d28c436acfc86e8f1736b604ee6f
luis fuentetaja <luisfuentetaja86@hotmail.com>
2018-09-05 12:52:43 +0200

    adelante

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
-const mysql = require ('mysql')
+const mysql = require('mysql')
 let pool = null
 
-exports.connect= (done)=>{
-pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database:'webturismo',
-})
-done()
+exports.connect = (done) => {
+    pool = mysql.createPool({
+        // host: '127.0.0.1',
+        // user: 'root',
+        // password: '',
+        // database:'webturismo',
+        host: 'eu-cdbr-west-02.cleardb.net',
+        user: 'bc98861819afa8',
+        password: '37b4811a',
+        database: 'heroku_b5b45e98e7b2de8',
+        port: 3306
+    })
+    done()
 }
-exports.get = () =>{
+exports.get = () => {
     return pool
 }
